07526f25a9dc0d3649660ded4be72c2dfae4091a
Oishe <yhsiokhan@gmail.com>
2026-02-21 00:00:20 +0600

    add eventlistener at the login button

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,17 @@
-console.log('js file connected successfully')
+// console.log('js file connected successfully');
+
+document.getElementById('login-btn').addEventListener('click', function(){
+    const inputNumber = document.getElementById('input-number');
+    const number = inputNumber.value;
+    // console.log(number);
+    const inputPin = document.getElementById('input-pin');
+    const pin = inputPin.value;
+    // console.log(pin);
+    if(number === '01234567890' && pin === '1234'){
+        alert('Login Successfully.');
+    }
+    else{
+            alert("Login Failed. Your number is 01234567890 and pin is 1234");
+            return;
+        }
+})
