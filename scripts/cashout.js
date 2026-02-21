c1919080e0f58695154529950c6be8a5c08e8e48
Oishe <yhsiokhan@gmail.com>
2026-02-21 19:42:58 +0600

    add functionality at the cashout btn, on processing

---
+++ scripts/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById("cashout-btn").addEventListener('click', function(){
+    const cashoutNumberInput = document.getElementById('cashout-number');
+    const agentNumber = cashoutNumberInput.value;
+
+    const cashoutAmountInput = document.getElementById('cashout-amount');
+    const cashoutAmount = cashoutAmountInput.value;
+
+    
+})
