5b2fafcb8bce06e6c0b737a2c6af8abcd64bd808
Oishe <yhsiokhan@gmail.com>
2026-02-21 22:28:49 +0600

    add onclick on the addMoney and cashOut button

--- scripts/machine.js
+++ scripts/machine.js
@@ -14,3 +14,13 @@ function setBalance(value){
     const balanceElement = document.getElementById('balance');
     balanceElement.innerText = value;
 }
+
+function showOnly(id){
+    const addMoney = document.getElementById('add-money');
+    const cashOut = document.getElementById('cash-out');
+    addMoney.classList.add('hidden');
+    cashOut.classList.add('hidden');
+
+    const selected = document.getElementById(id);
+    selected.classList.remove('hidden');
+}
